edca21ea7700d5b4445e5dc6ca9108aa801be37f
Kist <kistaaa@users.noreply.github.com>
2024-05-06 22:08:46 -0300

    Update 1
    Update modules
    Delete alpine.js
    Configure TW

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    container: {
-      center: true,
-    },
     extend: {
-      colors: {},
-    },
+      colors: {
+        bg: '#1f1f27',
+        grey1: '#2b2b37',
+        greyT: '#2b2b3799',
+        hover: '#00ffff',
+      },
+      grads: {
+        header: 'background:linear-gradient(180deg, #000 0%, #00000000)',
+        caption: 'background:linear-gradient(0deg, #2b2b37 0%, #2b2b3799)'
+      }
+    }
   },
   variants: {},
   plugins: [require("@tailwindcss/typography")],
